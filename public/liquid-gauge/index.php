98becd12ba77a91b2b165522d9a9705b8f29cb39
Jimmy Multani <jimmy.multani@twistimage.com>
2015-09-09 22:22:41 -0400

    reactify the liquid gauge plugin, add input box

--- public/liquid-gauge/index.php
+++ public/liquid-gauge/index.php
@@ -3,9 +3,10 @@
 <head>
     <script src="https://fb.me/react-0.13.3.min.js"></script>
     <script src="https://fb.me/JSXTransformer-0.13.3.js"></script>
+    <script src="http://d3js.org/d3.v3.min.js" language="JavaScript"></script>
     <script type="text/jsx" src="custom.js"></script>
 </head>
 <body>
-    <div id="react-container"></div>
+    <div id="react-root"></div>
 </body>
 </html>
